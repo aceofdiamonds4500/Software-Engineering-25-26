dfb9d10b57e6045a4d059f5cfe9f59d1fdf303a2
Chris Khun <97821198+ChrisKhun@users.noreply.github.com>
2025-09-03 16:57:32 -0400

    Delete pytorch directory

--- pytorch/torchtest.py
+++
@@ -1,7 +0,0 @@
-import torch
-x = torch.rand(5,3)
-print(x)
-
-print(torch.cuda.is_available()) #"is CUDA available for Nvidia drivers?"
-
-'''if this runs, your environment is running correctly :]'''
