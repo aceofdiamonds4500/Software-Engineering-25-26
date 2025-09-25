3d0a407441bc43b45952a7c8b712ff3d3aa362cc
Brian <xbrianmoore@gmail.com>
2025-09-24 23:11:55 -0400

    implemented training data for model

--- Analysis/read_file.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd #The pandas module. Used to convert structured data files (in our case, CSV) into dataframes, which are easier for our program to read and manipulate
-
-def pd_read(file):  #Simple function to convert our CSV file to a dataframe.
-    file_name = file
-    df = pd.read_csv(file_name)
-    return df
--- text_clean_and_insert.py
+++ text_clean_and_insert.py
@@ -1,7 +1,11 @@
-import re
+import re   #Regular Expression module. Used for the sub function below.
+                            
+def insert_clean_column(df, column_name, clean_column_name):    #df: dataframe to be edited, 
+                                                                #column_name: name of the column to be cleaned,
+                                                                #clean_column_name: name of the new column that will contain the cleaned values
 
-def insert_clean_column(df, column_name, clean_column_name):
-    df[clean_column_name] = list(map(lambda x:text_cleaner(x), df[column_name]))
+    df[clean_column_name] = list(map(lambda x:text_cleaner(x), df[column_name])) #Applies the text_cleaner method below to every value in a specified column, and assigns the values to a new column in the dataframe
 
-def text_cleaner(text):
-    return re.sub(r'[^\w]', ' ', str(text))
+def text_cleaner(text): #text: String that will be cleaned into alphanumeric characters.
+
+    return re.sub(r'[^\w]', ' ', str(text)) #Takes anything in the string that isn't an alphanumeric character and replaces it with a space.
