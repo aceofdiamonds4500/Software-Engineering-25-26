7b99727a414c7e1c3b316bd256c8b268a4d1256f
Brett Lawrence <brettlaw0111@gmail.com>
2025-11-03 10:06:35 -0500

    Created simple controller

--- GUI_SoftwareEng/obj/Debug/net8.0-windows/GUI_SoftwareEng.AssemblyInfo.cs
+++ GUI_SoftwareEng/obj/Debug/net8.0-windows/GUI_SoftwareEng.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI_SoftwareEng")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47445d850a86ce5e04669df0479e604456efc60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ac42da6614a3f3f6e050437fd4daafa7649e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI_SoftwareEng")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI_SoftwareEng")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
