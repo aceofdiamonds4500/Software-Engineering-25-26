eb3ea1c4abca8dffdd7790ecb8697c04d2a29a37
Alexander-W742 <aw361776@gmail.com>
2026-04-22 01:30:15 -0400

    added more to the register form and hid the side bar when on login form and fixed the register  page from making a new window

--- desktop/FormLogin.Designer.cs
+++ desktop/FormLogin.Designer.cs
@@ -34,6 +34,7 @@
             label2 = new Label();
             EmailTex = new TextBox();
             PasswordTex = new TextBox();
+            button3 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -41,7 +42,7 @@
             button1.AutoSize = true;
             button1.BackColor = Color.LightBlue;
             button1.Font = new Font("Microsoft Sans Serif", 12F);
-            button1.Location = new Point(267, 222);
+            button1.Location = new Point(229, 216);
             button1.Name = "button1";
             button1.Size = new Size(75, 30);
             button1.TabIndex = 0;
@@ -54,18 +55,19 @@
             button2.AutoSize = true;
             button2.BackColor = Color.LightBlue;
             button2.Font = new Font("Microsoft Sans Serif", 12F);
-            button2.Location = new Point(382, 222);
+            button2.Location = new Point(350, 216);
             button2.Name = "button2";
             button2.Size = new Size(79, 30);
             button2.TabIndex = 1;
             button2.Text = "Register";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 11.25F);
-            label1.Location = new Point(258, 111);
+            label1.Location = new Point(220, 105);
             label1.Name = "label1";
             label1.Size = new Size(49, 18);
             label1.TabIndex = 2;
@@ -75,7 +77,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Sans Serif", 11.25F);
-            label2.Location = new Point(228, 163);
+            label2.Location = new Point(190, 157);
             label2.Name = "label2";
             label2.Size = new Size(79, 18);
             label2.TabIndex = 3;
@@ -83,18 +85,34 @@
             // 
             // EmailTex
             // 
-            EmailTex.Location = new Point(313, 111);
+            EmailTex.Location = new Point(275, 105);
             EmailTex.Name = "EmailTex";
             EmailTex.Size = new Size(154, 23);
             EmailTex.TabIndex = 4;
             // 
             // PasswordTex
             // 
-            PasswordTex.Location = new Point(313, 163);
+            PasswordTex.Location = new Point(275, 157);
             PasswordTex.Name = "PasswordTex";
             PasswordTex.Size = new Size(154, 23);
             PasswordTex.TabIndex = 5;
             PasswordTex.UseSystemPasswordChar = true;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BackgroundImageLayout = ImageLayout.None;
+            button3.CausesValidation = false;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(220, 224, 228);
+            button3.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.Highlight;
+            button3.Location = new Point(435, 157);
+            button3.Name = "button3";
+            button3.Size = new Size(114, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Forgot Password?";
+            button3.UseCompatibleTextRendering = true;
+            button3.UseVisualStyleBackColor = false;
             // 
             // FormLogin
             // 
@@ -103,6 +121,7 @@
             AutoSize = true;
             BackColor = Color.FromArgb(220, 224, 228);
             ClientSize = new Size(700, 355);
+            Controls.Add(button3);
             Controls.Add(PasswordTex);
             Controls.Add(EmailTex);
             Controls.Add(label2);
@@ -123,5 +142,6 @@
         private Label label2;
         private TextBox EmailTex;
         private TextBox PasswordTex;
+        private Button button3;
     }
 }
